620d2554fa294a2821780e1eac619cfa23a8927a
rienihil <aluakural000@gmail.com>
2025-02-22 09:38:30 +0500

    .

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -12,4 +12,15 @@ router.get("/", authMiddleware, async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const { user_id, products } = req.body;
+    const newOrder = new Order({ user_id, products });
+    await newOrder.save();
+    res.status(201).json(newOrder);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 module.exports = router;
